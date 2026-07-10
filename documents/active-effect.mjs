2b382e3c67ad24fbd72f91ebf57bfe365b8cb4ed
konokonekonoko <50079394+konokonekonoko@users.noreply.github.com>
2026-07-10 12:52:13 +0200

    indentation

--- documents/active-effect.mjs
+++ documents/active-effect.mjs
@@ -3,113 +3,114 @@ import { GiftAbilityUnlocked, ListSortValueIncrement } from "../enums.mjs";
 const fields = foundry.data.fields;
 
 export class GiftAbilityData extends foundry.abstract.DataModel {
-  static defineSchema() {
-    return {
-      sort: new fields.NumberField({
-        initial: ListSortValueIncrement,
-        integer: true,
-        nullable: false,
-      }),
-      unlocked: new fields.NumberField({
-        initial: GiftAbilityUnlocked.Unlocked,
-        integer: true,
-        nullable: false,
-      }),
-      description: new fields.HTMLField(),
-      level: new fields.StringField({
-        initial: null,
-        nullable: true,
-      }),
-      resource: new fields.SchemaField({
-        name: new fields.StringField({
-          initial: null,
-          nullable: true,
-        }),
-        remaining: new fields.NumberField({
-          initial: null,
-          integer: false,
-          nullable: true,
-        }),
-        max: new fields.NumberField({
-          initial: null,
-          integer: false,
-          nullable: true,
-        }),
-      }),
-      cooldown: new fields.SchemaField({
-        remaining: new fields.NumberField({
-          initial: null,
-          integer: true,
-          nullable: true,
-        }),
-        max: new fields.NumberField({
-          initial: null,
-          integer: true,
-          nullable: true,
-        }),
-      }),
-    };
-  }
+    static defineSchema() {
+        return {
+            sort: new fields.NumberField({
+                initial: ListSortValueIncrement,
+                integer: true,
+                nullable: false,
+            }),
+            unlocked: new fields.NumberField({
+                initial: GiftAbilityUnlocked.Unlocked,
+                integer: true,
+                nullable: false,
+            }),
+            description: new fields.HTMLField(),
+            level: new fields.StringField({
+                initial: null,
+                nullable: true,
+            }),
+            resource: new fields.SchemaField({
+                name: new fields.StringField({
+                    initial: null,
+                    nullable: true,
+                }),
+                remaining: new fields.NumberField({
+                    initial: null,
+                    integer: false,
+                    nullable: true,
+                }),
+                max: new fields.NumberField({
+                    initial: null,
+                    integer: false,
+                    nullable: true,
+                }),
+            }),
+            cooldown: new fields.SchemaField({
+                remaining: new fields.NumberField({
+                    initial: null,
+                    integer: true,
+                    nullable: true,
+                }),
+                max: new fields.NumberField({
+                    initial: null,
+                    integer: true,
+                    nullable: true,
+                }),
+            }),
+        };
+    }
 }
 
 export class SentimentActiveEffect extends ActiveEffect {
-
-  #getParent() {
-    const item = this.parent;
-    if (!item) return;
-    const actor = item.parent;
-    if (!actor) return item;
-    return actor;
-  }
-
-  async abilityToChat() {
-    const context = {
-      title: "GiftAbility",
-      system: this.system
+    #getParent() {
+        const item = this.parent;
+        if (!item) return;
+        const actor = item.parent;
+        if (!actor) return item;
+        return actor;
     }
-    context.enrichedName =
-      await TextEditor.enrichHTML(this.name, {
-        secrets: this.isOwner,
-        async: true
-      });
-    context.enrichedDescription =
-      await TextEditor.enrichHTML(this.system.description, {
-        secrets: this.isOwner,
-        async: true
-      });
 
-    const templatePath = "systems/sentiment/templates/abilities/ability-chat.html"
-    return this.#renderToChatMessage(templatePath, context);
-  }
+    async abilityToChat() {
+        const context = {
+            title: "GiftAbility",
+            system: this.system,
+        };
+        context.enrichedName = await TextEditor.enrichHTML(this.name, {
+            secrets: this.isOwner,
+            async: true,
+        });
+        context.enrichedDescription = await TextEditor.enrichHTML(
+            this.system.description,
+            {
+                secrets: this.isOwner,
+                async: true,
+            }
+        );
 
-  /**
-   * Render an HTML template with arguments as a chat message with this character as the speaker.
-   * @param templatePath
-   * @param args
-   * @param messageOptions
-   * @private
-   */
-  async #renderToChatMessage(templatePath, args, messageOptions = {}) {
-    const html = await renderTemplate(templatePath, {
-      ...args,
-      speaker: this,
-    });
-    let message = {
-      user: game.user.id,
-      speaker: ChatMessage.getSpeaker({ actor: this.#getParent() }),
-      content: html,
-    };
+        const templatePath =
+            "systems/sentiment/templates/abilities/ability-chat.html";
+        return this.#renderToChatMessage(templatePath, context);
+    }
 
-    message = foundry.utils.mergeObject(message, messageOptions);
+    /**
+     * Render an HTML template with arguments as a chat message with this character as the speaker.
+     * @param templatePath
+     * @param args
+     * @param messageOptions
+     * @private
+     */
+    async #renderToChatMessage(templatePath, args, messageOptions = {}) {
+        const html = await renderTemplate(templatePath, {
+            ...args,
+            speaker: this,
+        });
+        let message = {
+            user: game.user.id,
+            speaker: ChatMessage.getSpeaker({ actor: this.#getParent() }),
+            content: html,
+        };
 
-    const createdMessage = await ChatMessage.create(message);
-    createdMessage.setFlag("sentiment", args.title ?? "Unknown", {
-      ...args,
-      origin: {
-        uuid: this.uuid,
-        name: this.name,
-      },
-    });
-    return createdMessage;
-  }
+        message = foundry.utils.mergeObject(message, messageOptions);
+
+        const createdMessage = await ChatMessage.create(message);
+        createdMessage.setFlag("sentiment", args.title ?? "Unknown", {
+            ...args,
+            origin: {
+                uuid: this.uuid,
+                name: this.name,
+            },
+        });
+        return createdMessage;
+    }
 }
--- sheets/gift-ability-sheet.mjs
+++ sheets/gift-ability-sheet.mjs
@@ -1,31 +1,31 @@
-import { 
-  GiftAbilityUnlocked,
-  reverseKeyValue
-  } from "../enums.mjs"
+import { GiftAbilityUnlocked, reverseKeyValue } from "../enums.mjs";
 
 export default class GiftAbilitySheet extends ActiveEffectConfig {
-
-  /** @override */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["sentiment", "sheet", "active-effect-sheet", "giftAbility"],
-      template: "systems/sentiment/templates/gift-ability-sheet.hbs",
-      width: 600,
-      height: 490
-    });
-  }
-
-  async getData(options) {
-    const context = await super.getData(options);
-
-    context.system = this.object.system;
-
-    context.flags = this.object.flags?.sentiment ?? {};
-  
-    context.selectOptions = {
-      unlocked: reverseKeyValue(GiftAbilityUnlocked)
+    /** @override */
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: [
+                "sentiment",
+                "sheet",
+                "active-effect-sheet",
+                "giftAbility",
+            ],
+            template: "systems/sentiment/templates/gift-ability-sheet.html",
+            width: 600,
+            height: 490,
+        });
     }
 
-    return context;
-  }
+    async getData(options) {
+        const context = await super.getData(options);
+
+        context.system = this.object.system;
+        context.flags = this.object.flags?.sentiment ?? {};
+
+        context.selectOptions = {
+            unlocked: reverseKeyValue(GiftAbilityUnlocked),
+        };
+
+        return context;
+    }
 }
