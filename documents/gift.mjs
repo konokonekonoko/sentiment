b2c9d58087d7812f2485a99b38a62bd3ecd53712
konokonekonoko <50079394+konokonekonoko@users.noreply.github.com>
2026-07-18 16:19:49 +0200

    Added field for gift type in gift sheets

--- documents/gift.mjs
+++ documents/gift.mjs
@@ -6,6 +6,7 @@ export class GiftData extends foundry.abstract.DataModel {
     static defineSchema() {
         return {
             description: new foundry.data.fields.HTMLField(),
+            type: new foundry.data.fields.StringField(),
             equipStatus: new foundry.data.fields.NumberField({
                 integer: true,
                 initial: GiftEquipStatusInitial
